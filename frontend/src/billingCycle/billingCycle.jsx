952f5ee12d1ed29aca460d3fefd89efa0f2cf3ea
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-29 18:27:26 -0200

    Componentes estruturais das abas

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsContent'
+import TabsContent from '../common/tab/tabsContent'
 
 export default class BillingCycle extends React.Component {
     render() {
@@ -10,7 +13,14 @@ export default class BillingCycle extends React.Component {
                     title='Ciclos de Pagamentos'
                     small='Cadastro' />
                 <Content>
-                    Ciclos de Pagamentos
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+                            
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
